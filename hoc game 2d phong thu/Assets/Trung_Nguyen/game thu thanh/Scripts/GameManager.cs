a6f691ad67f61f5f8ad7c4756b08e1756834719e
trungnguyen1221999 <trungnguyen1221999@gmail.com>
2025-02-19 14:16:11 +0200

    spawn enemy code

---
+++ hoc game 2d phong thu/Assets/Trung_Nguyen/game thu thanh/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Enemy[] enemyPrefabs;
+    public float spawnTime;
+    bool isGameOver;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine (SpawnEnemy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (!isGameOver)
+        {
+            int randomEnemy = Random.Range(0, enemyPrefabs.Length);
+            Enemy enemy = enemyPrefabs[randomEnemy];
+            Instantiate(enemy, new Vector2 (8,1), Quaternion.identity);
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
